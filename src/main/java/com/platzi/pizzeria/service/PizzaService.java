4f651c2b9810a518b76ac10ae4ce50c7f834f647
brejcha13320 <davidmejia-2000@outlook.com>
2024-08-16 23:45:51 -0500

    implementacion crud-repository

--- src/main/java/com/platzi/pizzeria/service/PizzaService.java
+++ src/main/java/com/platzi/pizzeria/service/PizzaService.java
@@ -1,9 +1,8 @@
 package com.platzi.pizzeria.service;
 
 import com.platzi.pizzeria.persistence.entity.PizzaEntity;
+import com.platzi.pizzeria.persistence.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,16 +10,31 @@ import java.util.List;
 @Service
 public class PizzaService {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final PizzaRepository pizzaRepository;
 
     @Autowired
-    public PizzaService(JdbcTemplate jdbcTemplate){
-        this.jdbcTemplate = jdbcTemplate;
+    public PizzaService(PizzaRepository pizzaRepository){
+        this.pizzaRepository = pizzaRepository;
     }
 
     public List<PizzaEntity> getAll(){
-        return this.jdbcTemplate.query("SELECT * FROM pizza", new BeanPropertyRowMapper<>(PizzaEntity.class));
+        return this.pizzaRepository.findAll();
     }
 
+    public PizzaEntity get(int idPizza){
+        return this.pizzaRepository.findById(idPizza).orElse(null);
+    }
+
+    public PizzaEntity save(PizzaEntity pizza){
+        return this.pizzaRepository.save(pizza);
+    }
+
+    public void delete(int idPizza){
+        this.pizzaRepository.deleteById(idPizza);
+    }
+
+    public boolean exists(int idPizza){
+        return this.pizzaRepository.existsById(idPizza);
+    }
 
 }
